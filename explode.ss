cd4f02454ac7c963456be84e1d6068e2ea5c0863
minimo <minimo.mobile@gmail.com>
2015-12-10 18:54:34 +0900

    add sample game

---
+++ explode.ss
@@ -0,0 +1,17 @@
+﻿{
+    "image": "explode",
+    "frame": {
+        "width": 32,
+        "height": 32,
+        "rows": 8,
+        "cols": 1
+    },
+    "animations" : {
+        "default": {
+            "frames": [0,1,2,3,4,5,6,7,8],
+            "next": "stop",
+            "frequency": 5
+        },
+    }
+}
+    
---
+++ sample3.js
@@ -0,0 +1,73 @@
+/*
+ * runstant
+ */
+
+phina.globalize();
+
+var ASSETS = {
+  image: {
+    'hiyoko': 'hiyoco_nomal_full.png',
+  },
+  spritesheet: {
+    'hiyoko_ss': 'hiyoko.ss',
+  },
+};
+
+phina.define('MainScene', {
+  superClass: 'CanvasScene',
+  
+  init: function() {
+    this.superInit();
+
+    //ひよこスプライト    
+    this.hiyoko = Hiyoko()
+      .addChildTo(this)
+      .setPosition(this.gridX.center(), this.gridY.center());
+
+    this.age = 0;
+  },
+  update: function() {
+  }
+});
+
+phina.define('Hiyoko', {
+  superClass: 'Sprite',
+  init: function() {
+    this.superInit('hiyoko', 32, 32);
+    this.setScale(3);
+
+    this.ss = FrameAnimation('hiyoko_ss');
+    this.ss.attachTo(this);
+    this.ss.gotoAndPlay('stop');
+
+    this.animationName = 'stop';
+
+    this.jump = false;    
+  },
+  update: function(app) {
+    var kb = app.keyboard;
+    if (kb.getKey('z') {
+        this.jump = true;
+        this.tweener.clear().to({y: 320}, wait, 'easeOutSine');
+    }
+  },
+});
+
+phina.define('Item', {
+  superClass: 'Sprite',
+  init: function(wait) {
+    this.superInit('Item', 32, 32);
+    this.frameIndex = Math.randint(0, 40);
+    this.tweener.clear().to({y: 320}, wait, 'easeOutSine');
+  },
+  update: function() {
+  },
+});
+
+phina.main(function() {
+  var app = GameApp({
+    startLabel: 'main',
+    assets: ASSETS,
+  });
+  app.run();
+});
